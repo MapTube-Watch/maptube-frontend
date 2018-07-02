47e35b1566acb17bb43229e0b9792a0fbc5056e8
Mohammed Zia <mohammed.zia33@gmail.com>
2018-07-02 18:44:22 +0200

    i18n implemented

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { DomComponent } from './dom/dom.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  //    return new PruningTranslationLoader(http);
+      return new TranslateHttpLoader(http);
+  }
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
